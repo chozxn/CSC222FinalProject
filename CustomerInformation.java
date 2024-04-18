f0bc93b80ecc33c9547f08b3b9ec429cc074a0e6
Christopher Johnson <158243860+chozxn@users.noreply.github.com>
2024-04-18 13:43:30 -0500

    Add files via upload

---
+++ CustomerInformation.java
@@ -0,0 +1,15 @@
+
+
+public class CustomerInformation {
+    
+    private String name;
+    private String address; 
+    private String phoneNumber;
+    private String email;
+    
+    
+    
+    
+    
+}
+
